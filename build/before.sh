a4620c85606dfbc023f13ff0a895b1054b1cb086
jcooky <bak723@gmail.com>
2013-02-14 16:55:01 +0900

    travis upload를 하기 위해서

---
+++ build/before.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mkdir -p .m2
+cd .m2
+wget https://gist.github.com/jcooky/8298ca697cb262e8724a/raw/7f8811ff8176f278f38a6594df40be05d9804138/settings-security.xml
+wget https://gist.github.com/jcooky/8298ca697cb262e8724a/raw/3ae6a341d6abc6a6d959c484f448c008cab726d0/settings.xml
