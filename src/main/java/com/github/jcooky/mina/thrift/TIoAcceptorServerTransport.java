caff1a6d63b101156c964e3f6b11d95a84d316be
jcooky <bak723@gmail.com>
2013-03-03 20:15:29 +0900

    NioSocketAcceptor -> IoAcceptor

--- src/main/java/com/github/jcooky/mina/thrift/TIoAcceptorServerTransport.java
+++ src/main/java/com/github/jcooky/mina/thrift/TIoAcceptorServerTransport.java
@@ -1,24 +1,20 @@
 package com.github.jcooky.mina.thrift;
 
-import com.github.jcooky.mina.thrift.codec.TFrameProtocolCodecFactory;
+import java.io.IOException;
+
 import org.apache.mina.core.service.IoAcceptor;
 import org.apache.mina.core.service.IoHandler;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
-import org.apache.mina.filter.executor.ExecutorFilter;
-import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 import org.apache.thrift.transport.TServerTransport;
 import org.apache.thrift.transport.TTransportException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
+import com.github.jcooky.mina.thrift.codec.TFrameProtocolCodecFactory;
 
 public class TIoAcceptorServerTransport extends TServerTransport {
 	public final static String CODEC_NAME = "mina-thrift-codec";
 	private IoAcceptor acceptor;
 
-	public TIoAcceptorServerTransport(NioSocketAcceptor acceptor) {
+	public TIoAcceptorServerTransport(IoAcceptor acceptor) {
 		if (!acceptor.getFilterChain().contains(CODEC_NAME))
 			acceptor.getFilterChain().addLast(CODEC_NAME, new ProtocolCodecFilter(new TFrameProtocolCodecFactory()));
 		this.acceptor = acceptor;
